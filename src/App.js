2ccb44a3fb8273670abb3a19c161f579fd2f5a54
Alireza <arbor0711@gmail.com>
2023-03-26 23:48:27 +0200

    some adjustment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import logo from "./logo.svg";
+import Nav from "./components/Nav";
+import Intro1 from "./components/Intro1";
+import side from "./components/Promo";
 import "./App.css";
 
-function Heading() {
-  return <h1>HEADING ONE</h1>;
-}
-``;
-function App() {
-  return <Heading />;
+// function Heading() {
+//   return <h1>HEADING ONE</h1>;
+// }
+// ``;
+function App(props) {
+  return <h1>{props.title}</h1>;
 }
 
 export default App;
