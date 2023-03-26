2aa2343b866aabf073ad5c4a55ec968f5e509a74
Alireza <arbor0711@gmail.com>
2023-03-27 00:26:57 +0200

    Passing props

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import Nav from "./components/Nav";
-import Intro1 from "./components/Intro1";
-import side from "./components/Promo";
-import "./App.css";
+import Heading from "./components/Heading";
 
-// function Heading() {
-//   return <h1>HEADING ONE</h1>;
-// }
-// ``;
-function App(props) {
-  return <h1>{props.title}</h1>;
+function App() {
+  return (
+    <div className="App">
+      <Heading firstName="qoli" />
+      <Heading firstName="Any name other than Bob" />
+    </div>
+  );
 }
 
 export default App;
