bc1f6576af07ba2a396ad0890eeac6f518a154de
Alireza <arbor0711@gmail.com>
2023-03-28 15:54:55 +0200

    Gussing number game

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import ModeToggler from "./components/ModeToggler";
 
 function App() {
-  return <ModeToggler />;
+  const handleClick = () => {
+    let randomNum = Math.floor(Math.random() * 3) + 1;
+    console.log(randomNum);
+    let userInput = prompt("Enter a number");
+    alert(`computer number was ${randomNum} and your gusse ${userInput}`);
+  };
+  return (
+    <div>
+      <h1>let's Play</h1>
+      <button onClick={handleClick}>Guess the number between 1 and 3</button>
+    </div>
+  );
 }
 
 export default App;
