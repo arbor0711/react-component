6c7d0ed136fb79a2fa545659ef1e8482d276478e
Alireza <arbor0711@gmail.com>
2023-04-12 11:54:26 +0200

    some changes

--- src/components/MealsList.js
+++ src/components/MealsList.js
@@ -1,11 +1,11 @@
-import {useState} from "react";
+import { useState } from "react";
 
-const MealsList= ()=>{
-    const todaysMeals=[
-        {name;"Beaked Beans", calories:200, id:123},
-        {name;"Grilled Veggies", calories:100, id:223},
-        {name;"Soup", calories:80, id:323},
-    ]
+const MealsList = () => {
+  const todaysMeals = [
+    { name: "Beaked Beans", calories: 200, id: 123 },
+    { name: "Grilled Veggies", calories: 100, id: 223 },
+    { name: "Soup", calories: 80, id: 323 },
+  ];
 
-    const [meals, setMeals]= useState(todaysMeals)
-}
+  const [meals, setMeals] = useState(todaysMeals);
+};
