489075ce78d83c4d783ddd1e0a725c36bc7e3172
Alireza <arbor0711@gmail.com>
2023-05-06 10:48:58 +0200

    4 types of events handler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import MealsProvider from "./components/providers/MealsProvider";
 import MealsList from "./components/MealsList";
 import Counter from "./components/Counter";
-
+import { Btn1, Btn2, Btn3, Btn4 } from "./components/Btn";
 function App() {
   return (
     <MealsProvider>
       <MealsList />
       <Counter />
+      <Btn1 />
+      <Btn2 />
+      <Btn3 />
+      <Btn4 />
     </MealsProvider>
   );
 }
--- src/components/btn.js
+++ src/components/btn.js
@@ -1,6 +1,41 @@
-function Btn() {
-  const clickHandler = () => console.log("I touched");
-  return <button onMouseOver={clickHandler}>Click Me</button>;
+const goodstyle = { backgroundColor: "green", color: "White" };
+const badstyle = { backgroundColor: "red", color: "white" };
+
+function Btn1() {
+  return (
+    <button
+      onClick={function () {
+        console.log("Btn1 clicked");
+      }}
+      style={badstyle}
+    >
+      inline anonymous ES5 function 
+    </button>
+  );
 }
 
-export default Btn;
+function Btn2() {
+  return (
+    <button onClick={() => console.log("Btn2 clicked")} style={goodstyle}>
+      inline anonymous ES6 functions
+    </button>
+  );
+}
+
+function Btn3() {
+  function OnClick() {
+    console.log("Btn3 clicked");
+  }
+  return <button onClick={OnClick}>separate function declarations</button>;
+}
+
+function Btn4() {
+  const OnClick = () => console.log("Btn4 clicked");
+  return (
+    <button onClick={OnClick} style={goodstyle}>
+      separate function expressions
+    </button>
+  );
+}
+
+export { Btn1, Btn2, Btn3, Btn4 };
